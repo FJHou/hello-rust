6c22acb94213d44cf50a616d60d8dafa77f82ad3
houfujian <houfujian@jd.com>
2023-02-08 13:19:11 +0800

    comiit

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,32 @@
-struct User {
-    name: String,
-    age: u32,
-    email: String,
-}
+/**!SECTION 1 */
 
-impl User {
-    fn say_hello(&self) {
-        println!("hello, i am {}, {} years old, my email is {}", self.name, self.age, self.email);
-    }
+// fn main() {
+//     let string1 = String::from("string1");
+//     println!("this is {}", string1);
 
-    fn change_age(&mut self, age: u32) {
-        self.age = age;
-    }
-}
+//     {
+//         let string2 = String::from("string2");
+//     }
+//     // 这里直接报错，原因和js表现一致：大括号的作用域限定了string2的使用范围
+//     println!("this is {}", string2);
+// }
+
+// fn main() {
+//     // 和js不同，rust的每个变量只有一个所有者，变量之间的赋值等于所有权的变更，不仅仅是值的复制
+//     let string1 = String::from("string1");
+//     let string2 = string1; // 变更所有权
+//     // 如果一个值又两个所有者，将会使rust的内存回收机制失效
+//     println!("string2 = {}", string2)
+// }
+
+/**!SECTION 举例描述rust内存安全，以及“如果一个值又两个所有者，将会使rust的内存回收机制失效”原因 */
 
 fn main() {
-    let mut someone = User {
-        name: String::from("hellen"),
-        age: 14,
-        email: String::from("hellen@gmail.com"),
-    };
+    let string1 = String::from("data");
+    {
+        // 下面的代码交换了string1的所有权到string2,当此行执行完毕后，string2的内存会被回收，所以打印string1的代码就会出错
+        let string2 = string1; // 所有权转移
+    }
 
-    someone.change_age(20);
-    someone.say_hello();
+    // println!("{}", string1)
 }
-
-// fn largest<T: std::cmp::PartialOrd>(a: T, b: T) -> T {
-//     if a > b {
-//         a
-//     } else {
-//         b
-//     }
-// }
-
-// fn main () {
-//     println!("{}", largest(1000, 2))
-// }
