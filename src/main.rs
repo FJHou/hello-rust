ac62b84d854d23e48f0774e775ccd80b8829dec1
houfujian <houfujian@jd.com>
2023-02-08 14:19:02 +0800

    feat: 借用和引用

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,11 @@
-struct User {
-    name: String,
-    age: u32,
-    email: String,
-}
-
-impl User {
-    fn say_hello(&self) {
-        println!("hello, i am {}, {} years old, my email is {}", self.name, self.age, self.email);
-    }
-
-    fn change_age(&mut self, age: u32) {
-        self.age = age;
-    }
+fn reverse_string(s: &mut String) -> String {
+    s.chars().rev().collect()
 }
 
 fn main() {
-    let mut someone = User {
-        name: String::from("hellen"),
-        age: 14,
-        email: String::from("hellen@gmail.com"),
-    };
+    let mut s1 = String::from("any string");
 
-    someone.change_age(20);
-    someone.say_hello();
+    let _s2 = reverse_string(&mut s1);
+
+    println!("{}", _s2)
 }
-
-// fn largest<T: std::cmp::PartialOrd>(a: T, b: T) -> T {
-//     if a > b {
-//         a
-//     } else {
-//         b
-//     }
-// }
-
-// fn main () {
-//     println!("{}", largest(1000, 2))
-// }
